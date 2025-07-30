92d1983cf0176f6f01aad46ba6c2c060faf87449
Rupesh Merotha <rupeshmerotha2004@gmail.com>
2025-07-30 15:52:21 +0530

    deployment issues fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,11 +5,16 @@ import userRoutes from "./routes/user.route.js"
 import chatRoutes from "./routes/chat.route.js"
 import { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser";
+import path from "path"
 import cors from "cors"
 const app = express()
 
 app.use(cookieParser());
 const PORT = process.env.PORT
+
+const __dirname = path.resolve()
+
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
@@ -20,6 +25,14 @@ app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
 app.use("/api/chat",chatRoutes)
 
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 
 
